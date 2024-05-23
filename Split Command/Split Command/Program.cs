e657522c44847c3dcd8bb756d772e51375773dc3
Arshid Ahmad <arshid.ahmad1@gmail.com>
2024-05-23 14:56:27 +0530

    FIrst Commit

---
+++ Split Command/Split Command/Program.cs
@@ -0,0 +1,12 @@
+﻿        // This program will split Full Name into First and Last Name
+        Console.WriteLine("Please enter your full name:");
+        string fullName = Console.ReadLine();
+
+        string[] names = fullName.Split(' ');
+        string firstName = names[0];
+        string lastName = names[1];
+
+        Console.WriteLine($"First Name: {firstName}");
+        Console.WriteLine($"Last Name: {lastName}");
+
+        Console.ReadLine();
